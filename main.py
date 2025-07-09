847b4fdf0cebd442d6c288979ec85750938b4531
Jhon Deivid Rojas Rodriguez <jhonrojasr253@gmail.com>
2025-07-08 21:25:53 -0500

    Aniadir mas limipeza de columnas

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
+from datetime import datetime
 import pandas as pd
-
+import matplotlib.pyplot as plt 
 
 df = pd.read_csv('dataset_vivienda.csv')
 
-print(df)
+anio = datetime.now().year
+
+df['anio_construccion'] = anio - df['antiguedad'] 
+
+df.drop('antiguedad', axis=1, inplace=True)
+
+#print(df.isnull().sum())
+
+#print(df.isnull().mean() * 100)
+print(df.head())
+#df.plot()
+#plt.show()
