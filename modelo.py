b61ce7fe85169713ebff18a7b61cc704d8e22eeb
Jhon Deivid Rojas Rodriguez <jhonrojasr253@gmail.com>
2025-07-14 03:04:10 -0500

    Organizacion de datos y columnas

--- modelo.py
+++ modelo.py
@@ -7,7 +7,7 @@ df = pd.read_csv("dataset_vivienda.csv")
 
 
 
-
+df.dropna(subset=['Descripcion'], inplace=False)
 
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
